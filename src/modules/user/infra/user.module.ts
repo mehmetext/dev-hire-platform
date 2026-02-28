ecc4fa61eb468f85928f8b3aa3a06c59356c0672
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-28 20:25:07 +0300

    feat: integrate UnitOfWorkModule into UserModule for improved transaction management

--- src/modules/user/infra/user.module.ts
+++ src/modules/user/infra/user.module.ts
@@ -1,23 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CandidateModule } from 'src/modules/candidate/infra/candidate.module';
 import { CompanyModule } from 'src/modules/company/infra/company.module';
-import { UnitOfWorkRepository } from 'src/shared/modules/unit-of-work/application/repositories/unit-of-work.repository';
-import { PrismaUnitOfWorkRepository } from 'src/shared/modules/unit-of-work/infra/prisma-unit-of-work.repository';
+import { UnitOfWorkModule } from 'src/shared/modules/unit-of-work/unit-of-work.module';
 import { UserRepository } from '../application/repositories/user.repository';
 import { CreateUserUseCase } from '../application/use-cases/create-user.use-case';
 import { PrismaUserRepository } from './db/prisma-user.repository';
 
 @Module({
   controllers: [],
-  imports: [CompanyModule, CandidateModule],
+  imports: [CompanyModule, CandidateModule, UnitOfWorkModule],
   providers: [
     {
       provide: UserRepository,
       useClass: PrismaUserRepository,
-    },
-    {
-      provide: UnitOfWorkRepository,
-      useClass: PrismaUnitOfWorkRepository,
     },
     CreateUserUseCase,
   ],
