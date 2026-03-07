1309dfcb53037b4b2ccdd82159c35683251f845e
Mehmet Konukçu <mehmetext68@gmail.com>
2026-03-07 13:46:17 +0300

    feat: add authentication and response documentation to upgrade subscription endpoint in SubscriptionController

--- src/modules/subscription/infra/http/subscription.controller.ts
+++ src/modules/subscription/infra/http/subscription.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiNoContentResponse } from '@nestjs/swagger';
 import { SubscriptionPlan } from 'src/modules/company/domain/enums/subscription-plan.enum';
 import { UserResponseDto } from 'src/modules/user/infra/dtos/user-response.dto';
 import { UpgradeCompanySubscriptionUseCase } from '../../application/use-cases/upgrade-company-subscription.use-case';
@@ -12,6 +13,8 @@ export class SubscriptionController {
   ) {}
 
   @Post('upgrade-to-pro')
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
   @UseGuards(AuthGuard('jwt'))
   async upgradeToPro(@Req() req: Request & { user: UserResponseDto }) {
     if (!req.user.companyProfile?.id) {
